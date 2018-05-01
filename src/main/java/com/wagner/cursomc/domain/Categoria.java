c70d1a78f81f9eaa12f33c18a49c4f34f0e09414
Wagner Salvador <hsswagner3@gmail.com>
2018-05-01 11:20:36 -0300

    Criado Base h2 e Adicionado anotações para que a Categoria já vire uma tabela

--- src/main/java/com/wagner/cursomc/domain/Categoria.java
+++ src/main/java/com/wagner/cursomc/domain/Categoria.java
@@ -1,9 +1,18 @@
 package com.wagner.cursomc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String nome;
 
     public Categoria(){
