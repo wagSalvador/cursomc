5ad69ae026aa1d72670749528ede847e5bc42db8
Wagner Salvador <hsswagner3@gmail.com>
2018-05-20 11:47:16 -0300

    Adicionando gravação de Categoria no bando ao iniciar o sistema

--- src/main/java/com/wagner/cursomc/CursomcApplication.java
+++ src/main/java/com/wagner/cursomc/CursomcApplication.java
@@ -1,12 +1,34 @@
 package com.wagner.cursomc;
 
+import com.wagner.cursomc.domain.Categoria;
+import com.wagner.cursomc.respositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import java.util.Arrays;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursomcApplication.class, args);
-	}
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursomcApplication.class, args);
+    }
+
+    /**
+     * Método para popular a base com dados ao ser iniciado o sistema.
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria categoria1 = new Categoria(null, "Informática");
+        Categoria categoria2 = new Categoria(null, "Escritório");
+        categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+
+    }
 }
