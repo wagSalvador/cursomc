75f57f431b531c5f6ce4e571981aa1d41d134add
wagner.salvador <wagner.salvador@senior.com.br>
2018-07-03 20:16:46 -0300

    Inserindo uma nova Categoria com o POST

--- src/main/java/com/wagner/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/wagner/cursomc/resources/CategoriaResource.java
@@ -1,13 +1,18 @@
 package com.wagner.cursomc.resources;
 
+import java.net.URI;
+
 import com.wagner.cursomc.domain.Categoria;
 import com.wagner.cursomc.services.CategoriaService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -21,6 +26,19 @@ public class CategoriaResource {
         Categoria categoria = categoriaService.search(id);
 
         return ResponseEntity.ok(categoria);
+    }
+
+    /**
+     * @RequestBody faz o Json ser convertido para objeto java
+     * @param categoria
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody  Categoria categoria) {
+        categoria = categoriaService.insert(categoria);
+        //pega a url de inserção
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/wagner/cursomc/services/CategoriaService.java
+++ src/main/java/com/wagner/cursomc/services/CategoriaService.java
@@ -3,6 +3,7 @@ package com.wagner.cursomc.services;
 import com.wagner.cursomc.domain.Categoria;
 import com.wagner.cursomc.respositories.CategoriaRepository;
 import com.wagner.cursomc.services.exceptions.ObjectNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,4 +20,7 @@ public class CategoriaService {
         return categoria.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class));
     }
 
+    public Categoria insert(Categoria categoria) {
+        return categoriaRepository.save(categoria);
+    }
 }
